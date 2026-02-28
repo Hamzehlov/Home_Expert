29fa2c0410f36e9f736534beb7b8686c71233129
hamzeh <hamzeh@DESKTOP-4PANGM0>
2026-02-28 06:26:21 -0800

    جديد

---
+++ Home_Expert/Controllers/ServiceController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Home_Expert.Controllers
+{
+    public class ServiceController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
